c24b8f2d2b1a71a624429692fb869388b142cd2b
kosei_iino <suteaka9470@yahoo.co.jp>
2021-01-21 14:51:30 +0900

    onAuthStateChanged時のオブジェクトの受け取り方法変更

--- src/store.js
+++ src/store.js
@@ -11,32 +11,32 @@ export default new Vuex.Store({
         }
     },
     mutations: {
-        userSave(state, data) {
-            state.user.userData = data;
+        userSave(state, userData) {
+            state.user.userData = userData;
         }
     },
     actions: {
         async signup(context, data) {
             try {
-                const res = await firebase.auth().createUserWithEmailAndPassword(data.mailAddress, data.password);
-                await res.user.updateProfile({ displayName: data.userName });
-                context.commit('userSave', res);
+                const userData = await firebase.auth().createUserWithEmailAndPassword(data.mailAddress, data.password);
+                await userData.user.updateProfile({ displayName: data.userName });
+                context.commit('userSave', userData);
             } catch (e) {
                 console.log(e);
             }
         },
         async login(context, data) {
             try {
-                const res = await firebase.auth().signInWithEmailAndPassword(data.mailAddress, data.password);
-                context.commit('userSave', res);
+                const userData = await firebase.auth().signInWithEmailAndPassword(data.mailAddress, data.password);
+                context.commit('userSave', userData);
             } catch (e) {
                 console.log(e);
             }
         },
         onAuth(context) {
             firebase.auth().onAuthStateChanged(user => {
-                user = user ? user : {};
-                context.commit('userSave', user);
+                const userData = user ? user : {};
+                context.commit('userSave', userData);
                 console.log(user);
             });
         }
